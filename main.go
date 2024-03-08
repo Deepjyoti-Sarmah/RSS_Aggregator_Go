74414f5d3335ba955e1d77326a59c147c48f1ef9
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-03-08 17:36:10 +0530

    adding sqlc and goose to create schema from raw sql queries and handling the User creating

--- main.go
+++ main.go
@@ -1,54 +1,73 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Deepjyoti-Sarmah/RSS_Aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-  // fmt.Println("RSS Aggrigator in Golang")
-  
-  godotenv.Load(".env")
+	// fmt.Println("RSS Aggrigator in Golang")
 
-  portString := os.Getenv("PORT")
-  if portString == "" {
-    log.Fatal("PORT is not found in the environment")
-  }
+	godotenv.Load(".env")
 
-  router := chi.NewRouter()
+	portString := os.Getenv("port")
+	if portString == "" {
+		log.Fatal("port is not found in the environment")
+	}
 
-  router.Use(cors.Handler(cors.Options{
-    AllowedOrigins: []string{"https://*", "http://*"},
-    AllowedMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
-    AllowedHeaders: []string{"*"},
-    ExposedHeaders: []string{"Link"},
-    AllowCredentials: false,
-    MaxAge: 300,
-  }))
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to Database")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
-  srv := &http.Server{
-    Handler: router,
-    Addr: ":"+portString,
-  }
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
 
-  log.Printf("Server starting on port %v", portString)
+	log.Printf("Server starting on port %v", portString)
 
-  err := srv.ListenAndServe()
-  if err != nil {
-    log.Fatal(err)
-  }
+	log.Fatal(srv.ListenAndServe())
 
-
-  // fmt.Println("PORT", portString)
+	// fmt.Println("PORT", portString)
 }
