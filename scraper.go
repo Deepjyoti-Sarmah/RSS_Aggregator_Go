68a62ed37186229db8fa24e6494e881c41042d7d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-03-15 17:10:55 +0530

    storing posts in database

--- scraper.go
+++ scraper.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
 	"github.com/Deepjyoti-Sarmah/RSS_Aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScapping(
@@ -52,7 +54,33 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("couldn't parse date %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		_,err = db.CreatePost(context.Background(), 
+			database.CreatePostParams{
+				ID: uuid.New(),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				Title: item.Title,
+				Description: description,
+				PublishedAt: pubAt,
+				Url: item.Link,
+				FeedID: feed.ID,
+			})
+		if err != nil {
+			log.Println("failed to create post", err)
+			return
+		}
 	}
-	log.Println("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
