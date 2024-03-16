20438b7ed8105d4c0701e4e92e11e1333a8afac4
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-03-16 17:14:49 +0530

    Adding post follow handler

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
@@ -78,9 +79,12 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 				FeedID:      feed.ID,
 			})
 		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key"){
+				continue
+			}
 			log.Println("failed to create post", err)
-			return
 		}
 	}
+
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
